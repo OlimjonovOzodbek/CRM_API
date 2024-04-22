c0f1109ee21cf131207b42dae7987b1ed378e802
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-22 15:29:08 +0500

    Add Infrastructure

---
+++ CRM_Auth.Infractructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using CRM_Auth.Infractructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Auth.Infractructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options
+                    .UseLazyLoadingProxies()
+                        .UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
