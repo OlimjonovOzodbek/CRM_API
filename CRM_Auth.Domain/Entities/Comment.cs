22ea58e1069cdc44eaf79f1527d2c5f4a5f1d596
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-22 15:18:46 +0500

    Entities

---
+++ CRM_Auth.Domain/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Auth.Domain.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Message { get; set; }
+        public Guid LessonId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ CRM_Auth.Domain/Entities/Lesson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Auth.Domain.Entities
+{
+    public class Lesson
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
